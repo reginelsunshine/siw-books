4b881d518aec7187e9ff8cbd03f7af4a35d2d413
Abdalla El Awad <abdaelawad@gmail.com>
2024-08-08 15:16:55 +0200

    sposta file

---
+++ siw-books/src/main/java/it/uniroma3/siw/SiwBookApplication.java
@@ -0,0 +1,14 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwBookApplication.class, args);
+
+	}
+
+}
