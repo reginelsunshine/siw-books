b000f131fa8e2554db7a78987dd1e0329c07500c
Abdalla El Awad <abdaelawad@gmail.com>
2024-08-08 16:00:10 +0200

    sposta file

--- siw-books/src/main/java/it/uniroma3/siw/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package it.uniroma3.siw.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import it.uniroma3.siw.model.Book;
-
-public interface BookRepository extends CrudRepository<Book,Long> {
-	
-}
