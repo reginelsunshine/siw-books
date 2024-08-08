20c5cfb4998eb27d765868fd9b9cf4ffb97c52e0
Abdalla El Awad <abdaelawad@gmail.com>
2024-08-08 15:13:48 +0200

    creazione repo

---
+++ src/main/java/it/uniroma3/siw/model/Book.java
@@ -0,0 +1,18 @@
+package it.uniroma3.siw.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String title;
+	private Integer year;
+	private String author;
+	
+	
+}
