420c17c0beef52a7136aee78c1ac5005c683e247
Abdalla El Awad <abdaelawad@gmail.com>
2024-08-17 15:43:56 +0200

    aggiunta due metodi bookController
    da controllare metodo save in BookService

--- src/main/java/it/uniroma3/siw/controller/BookController.java
+++ src/main/java/it/uniroma3/siw/controller/BookController.java
@@ -3,9 +3,12 @@ package it.uniroma3.siw.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
+import it.uniroma3.siw.model.Book;
 import it.uniroma3.siw.service.BookService;
 
 @Controller
@@ -22,5 +25,18 @@ public class BookController {
 	public String showBooks(Model model) {
 		model.addAttribute("books", this.bookService.findAll());
 		return "books.html";
+	}
+	
+	@GetMapping("/formNewBook")
+	public String formNewBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "formNewBook.html";
+	}
+	
+	@PostMapping("/book")
+	public String newBook(@ModelAttribute("book") Book book, Model model) {
+		this.bookService.save(book);
+		model.addAttribute("book", book);
+		return "book.html";
 	}
 }
