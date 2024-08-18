d93179b7990c940e8b9e34b0b7e0d037f8726c65
Abdalla El Awad <abdaelawad@gmail.com>
2024-08-18 23:17:55 +0200

    creazione import.sql

--- src/main/java/it/uniroma3/siw/model/User.java
+++ src/main/java/it/uniroma3/siw/model/User.java
@@ -1,5 +1,46 @@
 package it.uniroma3.siw.model;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
-}
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(name = "name")
+	private String name;
+
+	public User() {
+	}
+
+	public User(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}   
---
+++ src/main/resources/import.sql
@@ -0,0 +1,7 @@
+INSERT INTO book (id, title, author, year) VALUES 
+(1, '1984', 'George Orwell', 1949),
+(2, 'To Kill a Mockingbird', 'Harper Lee', 1960),
+(3, 'Brave New World', 'Aldous Huxley', 1932),
+(4, 'The Great Gatsby', 'F. Scott Fitzgerald', 1925),
+(5, 'Moby Dick', 'Herman Melville', 1851),
+(6, 'The Catcher in the Rye', 'J.D. Salinger', 1951);
