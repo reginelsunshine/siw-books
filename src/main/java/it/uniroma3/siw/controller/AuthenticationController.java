06e9609fa78c65234af6873c001cdb6d2b4ae7df
Abdalla El Awad <abdaelawad@gmail.com>
2024-08-20 18:11:04 +0200

    aggiunta possibilità di reindirizzamento automatico ad index dopo registrazione

--- src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
+++ src/main/java/it/uniroma3/siw/controller/AuthenticationController.java
@@ -82,6 +82,6 @@ public class AuthenticationController {
             model.addAttribute("user", user);
             return "registrationSuccessful";
         }
-        return "registerUser";
+        return "formRegisterUser";
     }
 }
