31d0096defe94a52828e6f66cf1d6969841e4dee
Abdalla El Awad <abdaelawad@gmail.com>
2024-08-22 19:41:19 +0200

    inserimento url_image in import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,15 +1,11 @@
-INSERT INTO author (id, name, surname, url_of_picture, date_of_birth) VALUES (nextval('hibernate_sequence'), 'George', 'Orwell', 'https://example.com/george_orwell.jpg', '1903-06-25');
-INSERT INTO author (id, name, surname, url_of_picture, date_of_birth) VALUES (nextval('hibernate_sequence'), 'Harper', 'Lee', 'https://example.com/harper_lee.jpg', '1926-04-28');
-INSERT INTO author (id, name, surname, url_of_picture, date_of_birth) VALUES (nextval('hibernate_sequence'), 'Aldous', 'Huxley', 'https://example.com/aldous_huxley.jpg', '1894-07-26');
-INSERT INTO author (id, name, surname, url_of_picture, date_of_birth) VALUES (nextval('hibernate_sequence'), 'F. Scott', 'Fitzgerald', 'https://example.com/f_scott_fitzgerald.jpg', '1896-09-24');
-INSERT INTO author (id, name, surname, url_of_picture, date_of_birth) VALUES (nextval('hibernate_sequence'), 'Herman', 'Melville', 'https://example.com/herman_melville.jpg', '1819-08-01');
+-- Inserimento degli autori
+INSERT INTO author (id, name, surname, url_of_picture, date_of_birth) VALUES 
+(nextval('hibernate_sequence'), 'George', 'Orwell', 'https://i.pinimg.com/originals/fa/84/da/fa84da00eb9cf1f0a1bceba90461b515.jpg', '1903-06-25'),
+(nextval('hibernate_sequence'), 'Harper', 'Lee', 'https://www.thefamouspeople.com/profiles/images/harper-lee-5.jpg', '1926-04-28'),
+(nextval('hibernate_sequence'), 'Aldous', 'Huxley', 'https://www.thefamouspeople.com/profiles/images/aldous-huxley-2.jpg', '1894-07-26');
 
-
-
-INSERT INTO book (id, title, url_image, year) VALUES (nextval('hibernate_sequence'), '1984', 'https://example.com/1984.jpg', 1949);
-INSERT INTO book (id, title, url_image, year) VALUES (nextval('hibernate_sequence'), 'To Kill a Mockingbird', 'https://example.com/mockingbird.jpg', 1960);
-INSERT INTO book (id, title, url_image, year) VALUES (nextval('hibernate_sequence'), 'Brave New World', 'https://example.com/brave_new_world.jpg', 1932);
-INSERT INTO book (id, title, url_image, year) VALUES (nextval('hibernate_sequence'), 'The Great Gatsby', 'https://example.com/gatsby.jpg', 1925);
-INSERT INTO book (id, title, url_image, year) VALUES (nextval('hibernate_sequence'), 'Moby Dick', 'https://example.com/moby_dick.jpg', 1851);
-
-
+-- Inserimento dei libri
+INSERT INTO book (id, title, url_image, year) VALUES 
+(nextval('hibernate_sequence'), '1984', 'https://www.stagemagazine.org/wp-content/uploads/2017/05/1984-poster.jpg', 1949),
+(nextval('hibernate_sequence'), 'To Kill a Mockingbird', 'https://cdn.waterstones.com/bookjackets/large/9781/7847/9781784752637.jpg', 1960),
+(nextval('hibernate_sequence'), 'Brave New World', 'https://m.media-amazon.com/images/I/81p56WUuDbL._AC_UF1000,1000_QL80_.jpg', 1932);
